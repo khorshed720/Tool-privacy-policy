8fd26fca581105e0360ecf4b6f3a63585dea89bc
khorshed720 <mdkhorshedalom573@gmail.com>
2026-06-26 22:35:13 +0600

    feat: switch to BrowserRouter and add SPA redirects
    Migrate from HashRouter to BrowserRouter to support cleaner URLs and add a _redirects file to ensure proper routing for the single-page application on GitHub Pages. Remove legacy deployment workflows.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PrivacyPolicy from "./PrivacyPolicy";
 import About from "./About";
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<PrivacyPolicy />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
