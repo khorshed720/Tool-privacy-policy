696f337321ba851997badb966d8ac75568707eb5
khorshed720 <mdkhorshedalom573@gmail.com>
2026-06-26 22:09:08 +0600

    refactor: switch to HashRouter for routing
    Replaced BrowserRouter with HashRouter to ensure compatibility with static hosting environments typically used for the privacy policy viewer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import PrivacyPolicy from "./PrivacyPolicy";
 import About from "./About";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<PrivacyPolicy />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
